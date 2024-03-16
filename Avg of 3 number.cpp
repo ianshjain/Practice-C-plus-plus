0c9270b98969135251d6e6268f14f63bd063af7a
ninjansh <142496574+ninjansh@users.noreply.github.com>
2024-03-16 21:17:26 +0530

    Create Avg of 3 number.cpp

---
+++ Avg of 3 number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+  float x,y,z,avg;
+  cout<<"Enter any 3 desired number- ";
+  cin>>x>>y>>z;
+  avg=(x+y+z)/3.0;
+  cout<<"Average is = "<<avg;
+  return 0;
+}
