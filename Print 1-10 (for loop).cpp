eed659320e682de6277395529eb04f944f646117
Ninjansh <142496574+ninjansh@users.noreply.github.com>
2024-03-16 22:11:11 +0530

    Create Print 1-10 (for loop).cpp

---
+++ Print 1-10 (for loop).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+  int i;
+  cout<<"The first 10 natural numbers are : \n";
+  for (i=1;i<=10;i++)
+  {
+  cout<<"\n"<<i;  
+  }
+  return 0;
+}
