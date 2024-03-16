2055a60648f1ca17ee1130f8551a2740676d2bdf
ninjansh <142496574+ninjansh@users.noreply.github.com>
2024-03-16 21:15:42 +0530

    Create Sum of 3 number.cpp

---
+++ Sum of 3 number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int x,y,z,a;
+  cout<<"Enter any 3 desired numbers";
+  cin>>x>>y>>z;
+    a=x+y+z;
+    cout<<"The addition of these given numbers are = "<<a;
+  return 0;
+}
