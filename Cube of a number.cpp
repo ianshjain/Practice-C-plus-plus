9aecd05611f70557bcafcd86d6acb4453e3f3233
Ninjansh <142496574+ninjansh@users.noreply.github.com>
2024-03-16 22:13:23 +0530

    Create Cube of a number.cpp

---
+++ Cube of a number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n,c;
+  cout<<"Enter any desired number : ";
+  cin>>n;
+  c=n*n*n;
+  cout<<"the cube of the number is : "<<c;
+  return 0;
+}
