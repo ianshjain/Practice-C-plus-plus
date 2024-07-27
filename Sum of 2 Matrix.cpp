25246bc5f23a1f9275f10ba79cab912e8c36103f
Ninjansh <142496574+ninjansh@users.noreply.github.com>
2024-07-27 22:04:37 +0530

    Update and rename 2-D Array.cpp to Sum of 2 Matrix.cpp

---
+++ Sum of 2 Matrix.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int rows, cols;
+    cout << "Enter the number of rows and columns: ";
+    cin >> rows >> cols;
+    int matrix1[rows][cols], matrix2[rows][cols], sum[rows][cols];
+    cout << "Enter elements of matrix 1:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> matrix1[i][j];
+        }
+    }
+    cout << "Enter elements of matrix 2:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> matrix2[i][j];
+        }
+    }
+    // Calculate sum of matrices
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sum[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+    cout << "Sum of matrices:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
